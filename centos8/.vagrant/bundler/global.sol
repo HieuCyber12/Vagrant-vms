4ffade3d17b71e95844d320c6d93ca8a0085cf34
HieuCyber <90963135+HieuDevA@users.noreply.github.com>
2023-08-09 06:40:45 +0700

    first commit

---
+++ centos8/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"452834388c1198582e14d545f4810a6668d77b4e82a8ee9070251508c38bb4db","vagrant_version":"2.3.4"}
---
+++ ubuntu18/devops/scrip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Welcom to Bash scripting"
+sudo apt update
+echo "Complete successfully"
